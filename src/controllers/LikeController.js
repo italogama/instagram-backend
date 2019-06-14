a84a82ea1f9f3ab98eb351ece03dbd68d630479e
italo.gama <italo.gama@mv.com.br>
2019-06-14 18:24:57 -0300

    Realização de upload,likes e resize das imagens

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,6 +2,14 @@ const Post = require('../models/Post');
 
 module.exports = {
     async store(req, res){
-        return res.json({ ok: true});
+        const post = await Post.findById(req.params.id);
+
+        post.likes += 1;
+
+        await post.save();
+
+        req.io.emit('like', post);
+        
+        return res.json(post);
     }
 };
